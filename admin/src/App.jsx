8eb9fd304b64185681906ddeb4a033892b4a141e
Vedant Patil <vedpatil5208@gmail.com>
2026-05-22 12:25:00 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-rkzf.onrender.com";
 
   return (
     <div>
